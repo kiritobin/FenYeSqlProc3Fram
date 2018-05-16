5d39be13b929edb234a88267c089130b0993f86e
daobin <38413720+kiritobin@users.noreply.github.com>
2018-05-16 11:20:31 +0800

    Update UserInfo.cs

--- WebApplication3/Model/UserInfo.cs
+++ WebApplication3/Model/UserInfo.cs
@@ -7,15 +7,14 @@ namespace WebApplication3.Model
 {
     public class UserInfo
     {
-        public UserInfo() { }
-        public string tableName = "stuInfo";
-        public string Column = "stuNO";
-        public int ColType = 0;
+        public string tableName { get; set; }
+        public string Column { get; set; }
+        public int ColType { get; set; }
         public int Order { get; set; }
-        public string Columnlist = "*";
+        public string Columnlist { get; set; }
         public int PageSize { get; set; }
         public int PageNum { get; set; }
-        public string Where = "";
-        public  string PageCount { get;set; }
+        public string Where { get; set; }
+        //public string PageCount { get; set; }
     }
 }
